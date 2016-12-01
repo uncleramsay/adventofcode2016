db79db5a0d9d7ad1e4d8656dfd75fd9a51fa075c
Dale Caffull <dcaffull@factset.com>
2016-12-01 19:23:53 +0000

    feat: slight refactor for readability

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const day = process.argv[2];
 const part = process.argv[3];
 
-const solutions = require(`${__dirname}/${day}/solutions`);
+const solutions = require(`./${day}/solutions`);
 const solution = solutions[part]();
 
 console.log(`Day ${day} Part ${part}: ${solution}`);
