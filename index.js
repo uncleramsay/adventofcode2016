ef1b23eaf7adc359d6337292cc5baed89737ff57
Dale Caffull <dcaffull@factset.com>
2016-12-01 18:40:13 +0000

    feat: scaffolding project structure

---
+++ index.js
@@ -0,0 +1,7 @@
+const day = process.argv[2];
+const part = process.argv[3];
+
+const solutions = require(`${__dirname}/${day}/solutions`);
+const solution = solutions[part]();
+
+console.log(`Day ${day} Part ${part}: ${solution}`);
