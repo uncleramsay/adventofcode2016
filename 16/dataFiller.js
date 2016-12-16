537f936ee89c00b0f44d3c93c9476e3872bc143f
Dale Caffull <dcaffull@factset.com>
2016-12-16 11:52:28 +0000

    feat(day 16): progress monitor

--- 16/dataFiller.js
+++ 16/dataFiller.js
@@ -13,9 +13,13 @@ module.exports = class DataFiller {
       let b = a.split('').reverse().join('');
       b = DataFiller.invertData(b);
       this.data = a + '0' + b;
+
+      this.displayDiskProgress();
     }
 
     this.data = this.data.substr(0, this.diskSize);
+    this.displayDiskProgress();
+    console.log();
   }
 
   getChecksum() {
@@ -27,9 +31,25 @@ module.exports = class DataFiller {
         newChecksum += (checksum[i] === checksum[i+1]) ? '1' : '0';
       }
       checksum = newChecksum;
+      this.displayChecksumProgress(checksum);
     }
 
+    console.log();
     return checksum;
+  }
+
+  displayDiskProgress() {
+    const percentage = (this.data.length / this.diskSize) * 100;
+
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Disk ${percentage.toFixed(0)}% full`);
+  }
+
+  displayChecksumProgress(checksum) {
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write(`Checksum length: ${checksum.length}`);
   }
 
   static invertData(data) {
